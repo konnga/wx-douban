d74d2b75e2f9b59c6d1fd982ba88cd022f45ffe7
‘wangjiacheng’ <wjc402402@163.com>
2018-04-13 11:08:35 +0800

    add: 配置路由

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -6,6 +6,10 @@ Page({
     logs: []
   },
   onLoad: function () {
+    wx.getStorageInfo({
+      success: (keys) => {
+        console.log(keys)
+      }})
     this.setData({
       logs: (wx.getStorageSync('logs') || []).map(log => {
         return util.formatTime(new Date(log))
