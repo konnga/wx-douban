7d2a391bee2fe783b7ed7967ca369337dc0499e5
‘wangjiacheng’ <wjc402402@163.com>
2018-04-18 13:37:35 +0800

    update: 更改loading组件样式

---
+++ components/loading/loading.js
@@ -0,0 +1,31 @@
+// components/loading/loading.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    load: {
+      type: Boolean,
+      value: true
+    },
+    loadText: {
+      type: String,
+      value: 'Loading...'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+  attached: function () {
+  },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
